a326551827a8011fb12c0f80a153634b0badee48
Tompic <95908721+Tommpic@users.noreply.github.com>
2021-12-10 10:41:06 +0000

    prova

---
+++ src/lib/tipi_numerici.py
@@ -0,0 +1,24 @@
+import math
+
+a= 12.456
+
+b= 5+ 10j
+
+c= math.pi
+print(a,b,c)
+print(type(a))
+print(type(b))
+print(b.real)
+
+print(-a//2)
+
+if (a>=c):
+  print('ottimo')
+
+else: 
+  print('nulla')
+
+
+d=False
+
+print(type(d))
